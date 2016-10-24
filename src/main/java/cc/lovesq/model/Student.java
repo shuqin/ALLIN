b065cea8aa459663d2d9296db280d5340c07410c
shuqin <shuqin@youzan.com>
2016-10-24 18:34:34 +0800

    加入 json 解析

---
+++ src/main/java/cc/lovesq/model/Student.java
@@ -0,0 +1,33 @@
+package cc.lovesq.model;
+
+/**
+ * Created by shuqin on 16/10/24.
+ */
+public class Student {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/cc/lovesq/result/BaseResult.java
@@ -0,0 +1,48 @@
+package cc.lovesq.result;
+
+/**
+ * Created by shuqin on 16/10/24.
+ */
+public class BaseResult<T> {
+
+    private int code = 200;
+    private String msg = "success";
+    private T data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public boolean isSuccess() {
+        return code == 200;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseResult{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
