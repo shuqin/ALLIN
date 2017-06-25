174427c6eb1ed082d31af13503d981109bb2e65a
shuqin <shuqin@youzan.com>
2017-06-25 20:58:28 +0800

    模拟柯里化

--- src/main/java/zzz/study/function/refactor/result/ConcurrentDataHandlerFrameRefactored.java
+++ src/main/java/zzz/study/function/refactor/result/ConcurrentDataHandlerFrameRefactored.java
@@ -24,6 +24,15 @@ public class ConcurrentDataHandlerFrameRefactored {
          (numbers) -> StreamUtil.map(numbers, (num) -> Math.sqrt(num)) );
     consumer(handledData, System.out::println);
 
+    List<Object> objs = StreamUtil.map(DataSupplier.getKeys(), s->Double.valueOf(s));
+    List<Double> handledData2 =
+        handleAllData((numbers) -> StreamUtil.map(numbers, (num) -> Math.pow((double)num,2))).apply(objs);
+    consumer(handledData2, System.out::println);
+
+    Function<List<String>, List<Object>> func = (numbers) -> StreamUtil.map(numbers, (num) -> Integer.parseInt(num)*2);
+    List<Object> handledData3 =
+        handleAllData(DataSupplier::getKeys).apply(func);
+    consumer(handledData3, System.out::println);
   }
 
   /**
@@ -41,6 +50,22 @@ public class ConcurrentDataHandlerFrameRefactored {
 
   public static <T,R> List<R> handleAllData(Supplier<List<T>> getAllKeysFunc, Function<List<T>, List<R>> handleBizDataFunc) {
     return handleAllData(getAllKeysFunc.get(), handleBizDataFunc);
+  }
+
+  /**
+   * 传入一个数据处理函数，返回一个可以并发处理数据集的函数, 该函数接受一个指定数据集
+   * Java 模拟柯里化: 函数工厂
+   */
+  public static <T,R> Function<List<T>, List<R>> handleAllData(Function<List<T>, List<R>> handleBizDataFunc) {
+    return ts -> handleAllData(ts, handleBizDataFunc);
+  }
+
+  /**
+   * 传入一个数据提供函数，返回一个可以并发处理获取的数据集的函数, 该函数接受一个数据处理函数
+   * Java 模拟柯里化: 函数工厂
+   */
+  public static <T,R> Function<Function<List<T>, List<R>>, List<R>> handleAllData(Supplier<List<T>> getAllKeysFunc) {
+    return handleBizDataFunc -> handleAllData(getAllKeysFunc.get(), handleBizDataFunc);
   }
 
   public static <T,R> List<R> handleAllData(List<T> allKeys, Function<List<T>, List<R>> handleBizDataFunc) {
