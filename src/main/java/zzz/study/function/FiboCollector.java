415739f93c8c637748c2d93e5d2b871cb112fbee
shuqin <shuqin@youzan.com>
2017-12-06 00:23:52 +0800

    增加斐波那契数列实现

---
+++ src/main/java/zzz/study/function/FiboCollector.java
@@ -0,0 +1,49 @@
+package zzz.study.function;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+/**
+ * Created by shuqin on 17/12/5.
+ */
+public class FiboCollector implements Collector<Integer, List<Integer>, List<Integer>> {
+
+  public Supplier<List<Integer>> supplier() {
+    return () -> {
+      List<Integer> result = new ArrayList<>();
+      result.add(0); result.add(1);
+      return result;
+    };
+  }
+
+  @Override
+  public BiConsumer<List<Integer>, Integer> accumulator() {
+    return (res, num) -> {
+      Integer next = res.get(res.size()-1) + res.get(res.size()-2);
+      res.add(next);
+    };
+  }
+
+  @Override
+  public BinaryOperator<List<Integer>> combiner() {
+    return (left, right) -> { left.addAll(right); return left; };
+  }
+
+  @Override
+  public Function<List<Integer>, List<Integer>> finisher() {
+    return res -> res;
+  }
+
+  @Override
+  public Set<Characteristics> characteristics() {
+    return Collections.emptySet();
+  }
+
+}
--- src/main/java/zzz/study/function/FunctionUtil.java
+++ src/main/java/zzz/study/function/FunctionUtil.java
@@ -1,9 +1,21 @@
 package zzz.study.function;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * Created by shuqin on 17/12/3.
@@ -23,6 +35,19 @@ public class FunctionUtil {
              list -> list.stream().sorted().collect(Collectors.toList()),
              list -> list.stream().sorted().map(Math::sqrt).collect(Collectors.toMap(x->x, y->Math.pow(2,y)))),
          Arrays.asList(64,49,25,16,9,4,1,81,36)));
+
+     List<Integer> list = Arrays.asList(1,2,3,4,5);
+     Supplier<Map<Integer,Integer>> mapSupplier = () -> list.stream().collect(Collectors.toMap(x->x, y-> y * y));
+
+     Map<Integer, Integer> mapValueAdd = list.stream().collect(Collectors.toMap(x->x, y->y, (v1,v2) -> v1+v2, mapSupplier));
+     System.out.println(mapValueAdd);
+
+     List<Integer> nums = Arrays.asList(Arrays.asList(1,2,3), Arrays.asList(1,4,9), Arrays.asList(1,8,27))
+                                .stream().flatMap(x -> x.stream()).collect(Collectors.toList());
+     System.out.println(nums);
+
+     List<Integer> fibo = Arrays.asList(1,2,3,4,5,6,7,8,9,10).stream().collect(new FiboCollector());
+     System.out.println(fibo);
    }
 
 }
