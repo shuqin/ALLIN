c5d6d896c78e2390ce1325be6213a91ac0e2fda5
shuqin <shuqin@youzan.com>
2017-12-12 22:44:44 +0800

    code improve

--- src/main/java/zzz/study/function/FunctionUtil.java
+++ src/main/java/zzz/study/function/FunctionUtil.java
@@ -1,8 +1,5 @@
 package zzz.study.function;
 
-import org.apache.commons.collections.CollectionUtils;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -17,9 +14,9 @@ import java.util.stream.Collectors;
  */
 public class FunctionUtil {
 
-   public static <T,R> List<R> multiGetResult(List<Function<List<T>, R>> functions, List<T> list) {
-     return functions.stream().map(f -> f.apply(list)).collect(Collectors.toList());
-   }
+  public static <T,R> List<R> multiGetResult(List<Function<List<T>, R>> functions, List<T> list) {
+    return functions.stream().map(f -> f.apply(list)).collect(Collectors.toList());
+  }
 
   public static <K,R> List<R> mergeList(List<R> srcList, List<R> destList ,
                                         Function<R,K> keyFunc,
@@ -40,6 +37,33 @@ public class FunctionUtil {
         }
     ).collect(Collectors.toList());
 
+  }
+
+  /** 对给定的值 x,y 应用指定的二元操作函数 */
+  public static <T,S,R> Function<BiFunction<T,S,R>, R> op(T x, S y) {
+    return opFunc -> opFunc.apply(x, y);
+  }
+
+  /** 将两个函数使用组合成一个函数，这个函数接受一个二元操作函数 */
+  public static <T,S,Q,R> Function<BiFunction<S,Q,R>, R> op(Function<T,S> funcx, Function<T,Q> funcy, T x) {
+    return opFunc -> opFunc.apply(funcx.apply(x), funcy.apply(x));
+  }
+
+  public static <T,S,Q,R> Function<BiFunction<S,Q,R>, Function<T,R>> op(Function<T,S> funcx, Function<T,Q> funcy) {
+    return opFunc -> aT -> opFunc.apply(funcx.apply(aT), funcy.apply(aT));
+  }
+
+  /** 将两个函数组合成一个叠加函数, compose(f,g) = f(g) */
+  public static <T> Function<T, T> compose(Function<T,T> funcx, Function<T,T> funcy) {
+    return x -> funcx.apply(funcy.apply(x));
+  }
+
+  /** 将若干个函数组合成一个叠加函数, compose(f1,f2,...fn) = f1(f2(...(fn))) */
+  public static <T> Function<T, T> compose(Function<T,T>... extraFuncs) {
+    if (extraFuncs == null || extraFuncs.length == 0) {
+      return x->x;
+    }
+    return x -> Arrays.stream(extraFuncs).reduce(y->y, FunctionUtil::compose).apply(x);
   }
 
    public static void main(String[] args) {
@@ -64,6 +88,12 @@ public class FunctionUtil {
 
      List<Integer> fibo = Arrays.asList(1,2,3,4,5,6,7,8,9,10).stream().collect(new FiboCollector());
      System.out.println(fibo);
+
+     System.out.println(op(new Integer(3), Integer.valueOf(3)).apply((x,y) -> x.equals(y.toString())));
+
+     System.out.println(op(x-> x.length(), y-> y+",world", "hello").apply((x,y) -> x+" " +y));
+
+     System.out.println(op(x-> x, y-> y+",world").apply((x,y) -> x+" " +y).apply("hello"));
 
    }
 
--- src/main/java/zzz/study/function/basic/Report.java
+++ src/main/java/zzz/study/function/basic/Report.java
@@ -9,34 +9,19 @@ import java.util.stream.Collectors;
 public class Report {
 
     public static void report(List<String> fields, List<Person> persons) {
-        List<String> titles = fields.stream().map(
+        String reportTitles = fields.stream().map(
                 field -> FieldConfAccompany.getInstance(field).getTitle()
-        ).collect(Collectors.toList());
-        String reportTitle = join(titles, ",");
+        ).collect(Collectors.joining(","));
 
         List<String> rows = persons.stream().map(
-                p -> {
-                    List<String> row = fields.stream().map(
-                            field -> FieldConfAccompany.getInstance(field).getMethod().apply(p)
-                    ).collect(Collectors.toList());
-                    return join(row, ",");
-                }
+                p -> fields.stream().map(
+                        field -> FieldConfAccompany.getInstance(field).getMethod().apply(p)
+                ).collect(Collectors.joining(","))
         ).collect(Collectors.toList());
 
-        System.out.println(reportTitle);
-        System.out.println(join(rows,"\n"));
+        System.out.println(reportTitles);
+        System.out.println(String.join("\n",rows));
 
-    }
-
-    public static String join(List<String> strs, String sep) {
-        StringBuilder sb = new StringBuilder();
-        if (strs == null || strs.size() == 0) { return ""; }
-        if (strs.size() == 1) {return strs.get(0); }
-        sb.append(strs.get(0));
-        for (int i=1; i< strs.size(); i++) {
-            sb.append(sep); sb.append(strs.get(i));
-        }
-        return sb.toString();
     }
 
 }
