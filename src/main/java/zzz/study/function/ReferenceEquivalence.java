43d64063210e3a523ac9fbc6b09e057d6af2f5fe
shuqin <shuqin@youzan.com>
2018-01-01 07:00:22 +0800

    改善嵌套map里字段驼峰转下划线程序

--- src/main/java/zzz/study/function/ReferenceEquivalence.java
+++ src/main/java/zzz/study/function/ReferenceEquivalence.java
@@ -10,15 +10,15 @@ public class ReferenceEquivalence {
     System.out.println("result = " + result1);
 
     Counting counting2 = new Counting(0);
-    int result2 = 5 + counting.inc(5);
+    int result2 = 5 + counting2.inc(5);
     System.out.println("result2 = " + result2);
 
     Counting counting3 = new Counting(0);
-    int result3 = counting.incInner(10,5) + counting.incInner(10, 5);
+    int result3 = counting3.incInner(10,5) + counting3.incInner(10, 5);
     System.out.println("result3 = " + result3);
 
     Counting counting4 = new Counting(0);
-    int result4 = 15 + counting.incInner(10, 5);
+    int result4 = 15 + counting4.incInner(10, 5);
     System.out.println("result4 = " + result4);
   }
 
