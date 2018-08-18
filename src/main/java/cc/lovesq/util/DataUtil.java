cb87023e90eaa124d11f1fee1ed9734f575be47a
shuqin <shuqin@youzan.com>
2018-08-18 20:58:29 +0800

    fix 时间曲线图绘制

---
+++ src/main/java/cc/lovesq/util/DataUtil.java
@@ -0,0 +1,16 @@
+package cc.lovesq.util;
+
+import java.text.DecimalFormat;
+
+public class DataUtil {
+
+  private static DecimalFormat df = new java.text.DecimalFormat("#.00");
+
+  /**
+   * 保留2位小数点
+   */
+  public static double retainNpoints(double d) {
+    return Double.parseDouble(df.format(d));
+  }
+
+}
