574f318cf0a1d7a57f7ad217498df0aa6ba016b7
shuqin <shuqin@youzan.com>
2019-01-05 17:13:29 +0800

    improve

--- src/main/java/zzz/study/apidesign/export/common/ExportParam.java
+++ src/main/java/zzz/study/apidesign/export/common/ExportParam.java
@@ -1,5 +1,7 @@
 package zzz.study.apidesign.export.common;
 
+import java.util.Map;
+
 import lombok.Data;
 import zzz.study.patterns.composite.escondition.Condition;
 
@@ -12,14 +14,24 @@ public class ExportParam {
   /** 导出业务类型，必传 */
   private String bizType;
 
+  /** 导出业务细分，必传 */
+  private String category;
+
+  /** 导出策略，默认 */
+  private String strategy = "standard";
+
   /** 搜索参数，必传 */
   private SearchParam search;
 
   /** 请求ID，必传 */
   private String requestId;
 
-  /** 导出ID, 运维使用 */
-  private Long exportId;
+
+  /** 导出选项，可用于定制化 */
+  private Map<String, Object> options;
+
+  /** 导出额外信息 */
+  private Map<String, String> extra;
 
 }
 
@@ -35,4 +47,7 @@ class SearchParam {
 
   /** 扩展搜索入参，可选 */
   private Condition condition;
+
+  /** 扩展搜索入参，供 REST 调用，DSL查询构建见 ... */
+  private String restCondition;
 }
