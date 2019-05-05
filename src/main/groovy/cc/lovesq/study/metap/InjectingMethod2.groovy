56be1723f406ee1ef23c6e7b45f2546d672bf61f
shuqin <shuqin@youzan.com>
2019-05-05 13:42:11 +0800

    improve groovy metap

--- src/main/groovy/cc/lovesq/study/metap/InjectingMethod2.groovy
+++ src/main/groovy/cc/lovesq/study/metap/InjectingMethod2.groovy
@@ -5,7 +5,9 @@ class InjectingMethod2 {
     static void main(args) {
 
         Map.metaClass.readVal = { path ->
-            if (delegate?.isEmpty || !path) { return  null }
+            if (delegate?.isEmpty || !path) {
+                return null
+            }
             def paths = path.split("\\.")
             def result = delegate
             paths.each { subpath ->
@@ -14,39 +16,11 @@ class InjectingMethod2 {
             result
         }
 
-        def skills = [id:123, name:'qin', 'skills': ['programming':'good', 'writing': 'good', 'expression':'not very good']]
-        println( skills.readVal('name') + " can do:\n" +
-                ['programming', 'writing', 'expression', 'dance'] .collect { "skills.$it" } .collect { "\t$it ${skills.readVal(it)}" }.join('\n') )
-
-        Map.metaClass {
-
-            flatMap = { ->
-                def finalResult = [:]
-                delegate.each { key, value ->
-                    if (value instanceof Map) {
-                        def innerMap = [:]
-                        value.each { k, v ->
-                            innerMap[key+'.'+k] = v
-                        }
-                        finalResult.putAll(delegate.flatMap(innerMap))
-                    }
-                    else {
-                        finalResult[key] = value
-                    }
-                }
-                finalResult
-            }
-
-            'static' {
-                pretty = {
-                    "[" + delegate.collect { it }.join(",") + "]"
-                }
-            }
-
-        }
-
-        println "pretty print: " + Map.pretty(skills)
-        println 'flatMap:' + skills.flatMap()
-
+        def skills = [id: 123, name: 'qin', 'skills': ['programming': 'good', 'writing': 'good', 'expression': 'not very good']]
+        println(skills.readVal('name') + " can do:\n" +
+                ['programming', 'writing', 'expression', 'dance'].collect { "skills.$it" }.collect {
+                    "\t$it ${skills.readVal(it)}"
+                }.join('\n'))
     }
+
 }
---
+++ src/main/groovy/cc/lovesq/study/metap/InjectingMethod3.groovy
@@ -0,0 +1,45 @@
+package cc.lovesq.study.metap
+
+class InjectingMethod3 {
+
+    static void main(args) {
+
+        Map.metaClass {
+
+            flatMap = { ->
+                def finalResult = [:]
+                delegate.each { key, value ->
+                    if (value instanceof Map) {
+                        def innerMap = [:]
+                        value.each { k, v ->
+                            innerMap[key+'.'+k] = v
+                        }
+                        finalResult.putAll(innerMap)
+                    }
+                    else {
+                        finalResult[key] = value
+                    }
+                }
+                finalResult
+            }
+
+            methodMissing = { name, margs ->
+                println "Unknown method=$name, args=$margs"
+            }
+
+            'static' {
+                pretty = {
+                    "[" + delegate.collect { it }.join(",") + "]"
+                }
+            }
+
+        }
+
+        def skills = [id:123, name:'qin', 'skills': ['programming':'good', 'writing': 'good', 'expression':'not very good']]
+
+        println "pretty print: " + Map.pretty(skills)
+        println 'flatMap:' + skills.flatMap()
+        println 'nonexist: ' + skills.nonexist()
+
+    }
+}
