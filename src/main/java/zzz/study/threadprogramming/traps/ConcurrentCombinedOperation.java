808000fe874a34ef11ab7b3d5496ec707e42ab6c
shuqin <shuqin@youzan.com>
2019-11-12 21:08:46 +0800

    锁优化

--- src/main/java/zzz/study/threadprogramming/traps/ConcurrentCombinedOperation.java
+++ src/main/java/zzz/study/threadprogramming/traps/ConcurrentCombinedOperation.java
@@ -2,27 +2,40 @@ package zzz.study.threadprogramming.traps;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ConcurrentCombinedOperation {
 
   private Map<String, AtomicLong> map = new ConcurrentHashMap<>();
 
+  private Lock lock = new ReentrantLock();
+
   public long addAndGet(String key) {
-    return map.getOrDefault(key, new AtomicLong()).getAndIncrement();
+    if(lock.tryLock()) {
+      try {
+        map.putIfAbsent(key, new AtomicLong());
+      } finally {
+        lock.unlock();
+      }
+    }
+    return map.get(key).incrementAndGet();
   }
 
   public long get(String key) {
     return map.get(key).get();
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     ConcurrentCombinedOperation concurrentCombinedOperation = new ConcurrentCombinedOperation();
     ThreadStarter.startMultiThreads(
         (ti) -> {
           System.out.println(ti + ":" + concurrentCombinedOperation.addAndGet("key"));
         }
     );
-    System.out.println("final: " + concurrentCombinedOperation.addAndGet("key"));
+    TimeUnit.SECONDS.sleep(3);
+    System.out.println("final: " + concurrentCombinedOperation.get("key"));
   }
 }
