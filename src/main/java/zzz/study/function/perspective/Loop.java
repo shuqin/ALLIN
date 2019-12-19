46d5a35457c28d07bc234c885a731113641ce96a
shuqin <shuqin@youzan.com>
2019-12-20 00:06:25 +0800

    循环函数式

--- src/main/java/zzz/study/function/perspective/Loop.java
+++ src/main/java/zzz/study/function/perspective/Loop.java
@@ -2,7 +2,7 @@ package zzz.study.function.perspective;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.function.Supplier;
 
 public class Loop {
@@ -25,24 +25,9 @@ public class Loop {
     System.out.println("func sum:" + loop(list, (x,y) -> x+y, () -> 0));
     System.out.println("func multiply: " + loop(list, (x,y) -> x*y, () -> 1));
 
-    System.out.println("func sum with generic:" + loopWithGeneric(list, (x,y) -> x+y, () -> 0));
-    System.out.println("func multiply with generic: " + loopWithGeneric(list, (x,y) -> x*y, () -> 1));
-
   }
 
-  public static Integer loop(List<Integer> list, BiFunction<Integer,Integer,Integer> bifunc, Supplier<Integer> supplier) {
-    Integer result = supplier.get();
-    for (int i=0; i < list.size(); i++) {
-      result = bifunc.apply(result, list.get(i));
-    }
-    return result;
-  }
-
-  public static <T> T loopWithGeneric(List<T> list, BiFunction<T,T,T> bifunc, Supplier<T> init) {
-    T result = init.get();
-    for (int i=0; i < list.size(); i++) {
-      result = bifunc.apply(result, list.get(i));
-    }
-    return result;
+  public static <T> T loop(List<T> list, BinaryOperator<T> bifunc, Supplier<T> init) {
+    return list.stream().reduce(init.get(), bifunc);
   }
 }
