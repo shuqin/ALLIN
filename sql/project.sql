a88e642757a3bec96059f4ddf0694bf918f76704
shuqin <shuqin_1984@163.com>
2020-11-08 05:46:38 +0800

    添加学生选课表

--- sql/project.sql
+++ sql/project.sql
@@ -1,6 +1,7 @@
 ## executed using root account
 ## mysql -uroot -p < /path/to/project.sql
 
+DROP USER 'test'@'localhost';
 drop database if exists test;
 
 CREATE USER 'test'@'localhost' IDENTIFIED BY 'test';
@@ -17,6 +18,23 @@ create table creative (
 	content varchar(50) not null,
 	submitter varchar(32) not null,
 	comment varchar(100) default '',
-	gmt_create datetime DEFAULT NULL,
-  	gmt_modified datetime DEFAULT NULL
+	gmt_create datetime DEFAULT CURRENT_TIMESTAMP,
+  	gmt_modified datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+drop table if exists student_courses;
+
+create table student_courses (
+    id int(10) UNSIGNED not null primary key AUTO_INCREMENT comment 'AUTO_INCREMENT ID',
+	s_id varchar(64) not null comment 'student ID',
+	t_id varchar(64) not null comment 'teacher ID',
+	room varchar(64) not null comment 'room name',
+	c_id varchar(32) not null comment 'course ID',
+	c_time int(10) not null comment 'course time',
+	extra varchar(256) default '' comment 'extra info',
+	gmt_create datetime DEFAULT CURRENT_TIMESTAMP,
+  	gmt_modified datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `student_courses` ADD INDEX stc_index ( `s_id`, `t_id`, `c_id` );
+ALTER TABLE `student_courses` ADD INDEX tc_index ( `t_id`, `c_id` );
